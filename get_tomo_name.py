53b5b0e4e157ecb72cdbc4f57762319615551028
IND-UCSF cryoEM facility <140845385+indcryoem@users.noreply.github.com>
2023-09-08 16:48:27 -0700

    Update get_tomo_name.py
    changed previous script by adding ts_path. This version works with the 4 block on-th-fly processing for scipion.

--- get_tomo_name.py
+++ get_tomo_name.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 def get_tomo_name(scipion_folder):
     
+    def get_tomo_name(scipion_folder):
+    
     scipion_abs_path = os.path.abspath(scipion_folder)
     scipion_runs = scipion_abs_path + '/Runs'
         
@@ -11,16 +13,16 @@ def get_tomo_name(scipion_folder):
     
     movie_subfolder = scipion_runs + '/' + process_dir[0] + '/extra'
     mcor2_subfolder = scipion_runs + '/' + process_dir[1] + '/extra'
+    ts_subfolder = scipion_runs + '/' + process_dir[2] + '/extra'
     aretomo_subfolder = scipion_runs + '/' + process_dir[-1] + '/extra'
     
     tomo_name = sorted([name for name in os.listdir(aretomo_subfolder) if os.path.isdir(os.path.join(aretomo_subfolder, name))])
     
-    scipion_stru = pd.DataFrame((),columns=["import_movie_path","motioncor_path","aretomo_path","tomo_name"])
+    scipion_stru = pd.DataFrame((),columns=["import_movie_path","motioncor_path","ts_path","aretomo_path","tomo_name"])
     scipion_stru["tomo_name"]=tomo_name
+    scipion_stru["ts_path"]=ts_subfolder
     scipion_stru["import_movie_path"]=movie_subfolder
     scipion_stru["motioncor_path"]=mcor2_subfolder
     scipion_stru["aretomo_path"]=aretomo_subfolder
     
-    return scipion_stru
-    
-    
+    return scipion_stru    
